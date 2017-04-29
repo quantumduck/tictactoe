197b79e0736c4c0c18d2c5a3d72688c1d4a86f37
quantumduck <quantum.duck@hotmail.com>
2017-04-28 20:23:55 -0400

    attempt to improve and generalize coordinate functions

--- js/main.js
+++ js/main.js
@@ -9,12 +9,38 @@ var xStart = 1;
 var xSkip = 4;
 var yStart = 0;
 var ySkip = 2;
-var xDivisions = 3;
-var yDivisions = 3;
 
-$(function() {
-  $('body').css('max-width', $('#game').css('width'));
+var userChar = 'X';
+var aiChar = 'O';
+var turnNum = 1;
+
+function drawBoard() {
+  var text = "";
+  for (y = 0; y < gameBoard.length; y++) {
+    for (x = 0; x < gameBoard[0].length; x++) {
+      if (gameBoard[y][x] === ' ') {
+        text += '&nbsp;'
+      } else {
+        text += gameBoard[y][x];
+      }
+    }
+    text += '<br>';
+  }
+  $('#game').html(text);
+}
+
+function getX(rawX) {
   var width = numFromPixels($('#game').css('width'));
+  var offset_left = numFromPixels($('body').css('margin-left'));
+  var x = Math.floor(gameBoard[0].length * (rawX - offset_left) / width);
+  console.log(x);
+  if ((x % xSkip) === (xSkip - 1)) {
+    return false;
+  }
+  return Math.floor(x / xSkip);
+}
+
+function getY(rawY) {
   var height =  numFromPixels($('#game').css('height'));
   var offset_top = sumNumFromPixels([
     $('body').css('margin-top'),
@@ -22,21 +48,12 @@ $(function() {
     $('h1').css('margin-top'),
     $('h1').css('margin-bottom'),
   ]);
-  var offset_left = numFromPixels($('body').css('margin-left'));
-  setTimeout(drawBoard, 500);
-  $('#game').on('click', function(e) {
-    var x = Math.floor(xDivisions * (e.clientX - offset_left) / width);
-    var y = Math.floor(yDivisions * (e.clientY - offset_top) / height);
-    addChar('X', x, y);
-  });
-});
-
-function drawBoard() {
-  var text = "";
-  for (y = 0; y < gameBoard.length; y++) {
-    text += gameBoard[y] + '\n';
+  var y = Math.floor(gameBoard.length * (rawY - offset_top) / height);
+  console.log(y);
+  if ((y % ySkip) === (ySkip - 1)) {
+    return false;
   }
-  $('#game').text(text);
+  return Math.floor(y  / ySkip);
 }
 
 function numFromPixels(string) {
@@ -52,10 +69,37 @@ function sumNumFromPixels(strings) {
 }
 
 function addChar(char, x, y) {
+  // console.log(char + x + y);
   x = xStart + (x * xSkip);
   y = yStart + (y * ySkip);
   var line = gameBoard[y];
+  console.log(line)
   line = line.substring(0, x) + char + line.substring(x + 1, line.length);
   gameBoard[y] = line;
   drawBoard();
 }
+
+function charAt(x, y) {
+  return gamdBoard[yStart + (y * ySkip)][Start + (x * xSkip)];
+}
+
+function ai() {
+  if (charAt(1, 1) === ' ') {
+    addChar(aiChar, 1, 1);
+  } else {
+    switch(turnNum) {}
+  }
+}
+
+$(function() {
+  $('body').css('max-width', $('#game').css('width'));
+  setTimeout(drawBoard, 500);
+  $('#game').on('click', function(e) {
+    x = getX(e.clientX);
+    y = getY(e.clientY);
+    if (x && y) {
+      addChar(userChar, x, y);
+      turnNum++;
+    }
+  });
+});
